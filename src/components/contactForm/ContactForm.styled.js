e66b90e8f62a88278cd78786a4ed3f583ab37ce6
MaximeWishnevski <velolivefan@gmail.com>
2022-08-06 09:39:13 +0300

    add

---
+++ src/components/contactForm/ContactForm.styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  width: auto;
+  height: auto;
+  display: block;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+`;
